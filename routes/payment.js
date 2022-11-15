1857059ad855d1ed3b7f511958ba14919288da96
Léa Castanet <lea@mbp-de-lea.lan>
2022-11-15 17:54:03 +0100

    ajout route payment

---
+++ routes/payment.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const router = require("./user");
+const stripe = require("stripe")(process.env.STRIPE_API_SECRET);
+process.env.STRIPE_API_SECRET;
+
+const isAuthenticated = require("../middlewares/isAuthenticated");
+
+router.post("/payment", isAuthenticated, async (req, res) => {
+  console.log(req.body);
+  try {
+    const stripeToken = req.body.stripeToken;
+    const response = await stripe.charges.create({
+      amount: (req.body.amount * 100).toFixed(2),
+      currency: "eur",
+      description: `Paiement sur vinted pour: ${req.body.title}`,
+      source: stripeToken,
+    });
+    console.log(response);
+
+    res.json(response.status);
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;
